4a1a9a90d9c1d17e115b1dc72278ac85fff5fb66
Friedemann, Julian <julian.friedemann@haw-hamburg.de>
2021-10-01 20:47:14 +0200

    Initial Commit | first Endpoint

---
+++ app.py
@@ -0,0 +1,32 @@
+import requests
+from flask import Flask
+
+app = Flask(__name__)
+
+baseRoute = "/nba_stats/api/v1"
+
+
+@app.route(baseRoute + '/playerstats_pg')
+def playerStatsPG():
+    headers = {
+        'Connection': 'keep-alive',
+        'Accept': 'application/json, text/plain, */*',
+        'x-nba-stats-token': 'true',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
+        'x-nba-stats-origin': 'stats',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-Mode': 'cors',
+        'Referer': 'https://stats.nba.com/',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9',
+    }
+    ## Todo Season in Req
+    player_info_url = 'https://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=2020-21&SeasonSegment=&SeasonType=Regular Season&ShotClockRange=&StarterBench=&TeamID=0&TwoWay=0&VsConference=&VsDivision=&Weight='
+
+    response = requests.get(url=player_info_url, headers=headers).json()
+
+    return response
+
+
+if __name__ == '__main__':
+    app.run()
